01133c944357639ac47a74d2697ebbaff088ec0d
Deplep <131609777+Deplep@users.noreply.github.com>
2025-04-30 11:53:51 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,10 +1,5 @@
 public class CheckDigit 
 {   
-  /** Returns the check digit for num, as described in part (a).  
-   *  Precondition: The number of digits in num is between one and    
-   *  six, inclusive.  
-   *          num >= 0  
-   */  
    public static int getCheck(int num) 
    {  
      int multiplier = 7;
